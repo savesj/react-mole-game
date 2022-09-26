122b5fc95e49858366d0e1b824feecc908c23185
savesj <jesuslares.20@hotmail.com>
2022-09-26 17:45:21 -0400

    Using useEffect

--- src/Components/EmptySlot.js
+++ src/Components/EmptySlot.js
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
 import MoleHill from '../molehill.png';
 
-const EmptySlot = (props) => {}
-
+const EmptySlot = (props) => {
+  useEffect(() => {
+    let randSeconds = Math.call(Math.random() * 5000);
+    let timer = setTimeout(() => {
+      props.toggle(true);
+    }, randSeconds);
+    return () => clearTimeout(timer);
+  });
+};
 export default EmptySlot;
